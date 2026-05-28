f57ec4e94aa06a8c02585a08bccb9c3096736201
Abhiram TK <abhiramtksuresh@gmail.com>
2026-05-28 10:36:46 +0530

    Added password hashing utilities

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
+
 from app.database import engine, Base
 
 from app.models.user import User
 from app.models.role import Role
 from app.models.permission import Permission
 
+from app.core.security import (hash_password, verify_password)
+
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 @app.get("/")
-def test_connection():
-    return {"message": "RBAC schema setup successful"}
+def home():
+
+    password = "mypassword123"
+    hashed_password = hash_password(password)
+    verified = verify_password(password, hashed_password)
+
+    return {"plain_password": password, "hashed_password": hashed_password, "verified": verified}

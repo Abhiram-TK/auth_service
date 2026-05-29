c94c6889e327d7e320f453b7b0c921f7a2a679c8
Abhiram TK <abhiramtksuresh@gmail.com>
2026-05-29 18:30:07 +0530

    Added centralized configuration to auth service

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+class Settings:
+
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",30))
+
+settings = Settings()

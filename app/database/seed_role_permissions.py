48579394daae6bcb0bb381cade4e57f8a4fb98fc
Abhiram TK <abhiramtksuresh@gmail.com>
2026-06-16 16:45:37 +0530

    Expanded role permission mappings

--- app/database/seed_role_permissions.py
+++ app/database/seed_role_permissions.py
@@ -7,8 +7,34 @@ from app.models.permission import Permission
 ROLE_PERMISSION_MAPPING = {
 
     "viewer": ["view_transactions", "view_inventory"],
-    "recruiter": ["view_transactions", "create_transactions", "view_inventory", "reserve_inventory"],
-    "admin": ["view_transactions", "create_transactions", "update_transactions", "view_inventory", "reserve_inventory", "dispatch_inventory"]}
+
+    "recruiter": ["view_transactions", "create_transactions",
+                  "view_inventory", "reserve_inventory"],
+
+    "support": ["view_users", "view_transactions",
+                "view_inventory"],
+
+    "auditor": ["view_transactions", "view_inventory",
+                "audit_inventory", "export_transactions"],
+
+    "manager": ["view_transactions", "create_transactions",
+                "update_transactions", "approve_transactions",
+                "view_users", "update_users",
+                "view_roles"],
+
+    "admin": ["view_transactions", "create_transactions",
+              "update_transactions", "delete_transactions",
+              "approve_transactions", "cancel_transactions",
+              "export_transactions", "view_inventory",
+              "reserve_inventory", "dispatch_inventory",
+              "receive_inventory", "adjust_inventory",
+              "audit_inventory", "view_users",
+              "create_users", "update_users",
+              "disable_users", "view_roles",
+              "assign_roles", "view_permissions",
+              "assign_permissions"]
+
+}
 
 def seed_role_permissions():
 
